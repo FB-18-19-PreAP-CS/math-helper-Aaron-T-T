cfde34ddeb3c652b02840d186e723499a751daf9
TORRES.AARON <2210153@fbisd.flourbluffschools.net>
2018-09-17 14:10:33 -0500

    doctest for slope formula

--- math_helper_AaronT.py
+++ math_helper_AaronT.py
@@ -1,10 +1,34 @@
 from math import *
 
-def slope(y1,y2,x1,x3):
-    y1 = float(input("Give y1"))
-    y2 = float(input("Give y2"))
-    x1 = float(input("Give x1"))
-    x2 = float(input("Give x2"))
+def slope(y1,y2,x1,x2):
+#    y1 = float(input("Give Y1: "))
+#    y2 = float(input("Give Y2: "))
+#    x1 = float(input("Give X1: "))
+#    x2 = float(input("Give X2: "))
+    ''' Gets the inputs for the y1,y2,x1,and x2
+        
+        >>> slope(7,2,2,9)
+        -5/7
+        >>> slope(7,8,3,5)
+        1/2
+        >>> slope(1,3,9,2)
+        2/-7
+            
+    
+    '''
+    a = y2-y1
+    b = x2 -x1
+    
+    print(f"{a}/{b}")
+    
+    
 
 
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    
+    
     
