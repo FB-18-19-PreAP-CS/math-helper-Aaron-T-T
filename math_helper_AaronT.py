308796c922ebb553d5dab12690b67af51dcee1e9
TORRES.AARON <2210153@fbisd.flourbluffschools.net>
2018-09-17 13:29:41 -0500

    Friday formula

--- math_helper_AaronT.py
+++ math_helper_AaronT.py
@@ -1,1 +1,10 @@
-a
+from math import *
+
+def slope(y1,y2,x1,x3):
+    y1 = float(input("Give y1"))
+    y2 = float(input("Give y2"))
+    x1 = float(input("Give x1"))
+    x2 = float(input("Give x2"))
+
+
+    
